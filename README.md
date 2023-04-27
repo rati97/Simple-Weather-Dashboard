7d3c4d63926d207d9b951fe4db767feaf3fcd2a4
Rati Chkhetia <rchkhetia25@gmail.com>
2023-04-27 18:10:55 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Simple Web Weather Dashboard
+
+Weather dashboard created using HTML/CSS/Javascript and OpenWeatherMap API.
+
+Follow the steps below to set up the project on your local machine:
+
+1. Clone the repository
+2. Obtain an API key for the OpenWeatherMap API:
+
+- Visit [https://openweathermap.org/](https://openweathermap.org/)
+- Sign up for a free account or log in to your existing account
+- Go to the [API keys section](https://home.openweathermap.org/api_keys) in your dashboard
+- Generate a new API key or use an existing one
+
+3. Create a `config.js` file in the root directory of the project
+
+Open the `config.js` file and add the following content:
+
+```javascript
+const config = {
+  OPEN_WEATHER_API_KEY: "your_api_key_here",
+};
+```
+
+4. Open the index.html file in your preferred web browser.
---
+++ script.js
@@ -0,0 +1,48 @@
+const apiKey = config.OPEN_WEATHER_API_KEY;
+
+
+// function for getting weather data based on location
+async function getWeatherData(city, stateCode, countryCode) {
+    const query = `${city},${stateCode ? stateCode + ',' : ''}${countryCode}`;
+    const geocodeUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=${apiKey}`;
+  
+    try {
+      const geocodeResponse = await fetch(geocodeUrl);
+      if (!geocodeResponse.ok) {
+        throw new Error(`Error fetching geocoding data: ${geocodeResponse.status}`);
+      }
+      const geocodeData = await geocodeResponse.json();
+      if (geocodeData.length === 0) {
+        throw new Error(`No coordinates found for the given location.`);
+      }
+      const { lat, lon } = geocodeData[0];
+  
+      const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+      const weatherResponse = await fetch(weatherUrl);
+      if (!weatherResponse.ok) {
+        throw new Error(`Error fetching weather data: ${weatherResponse.status}`);
+      }
+      const weatherData = await weatherResponse.json();
+      return weatherData;
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+
+// in case of US, we need to show state code as well
+const countrySelect = document.getElementById("country");
+const stateContainer = document.getElementById("state-container");
+
+countrySelect.addEventListener("change", (event) => {
+if (event.target.value === "US") {
+    stateContainer.style.display = "block";
+} else {
+    stateContainer.style.display = "none";
+}
+});
+  
+
+
+
+  
